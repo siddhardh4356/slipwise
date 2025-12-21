4705d33420389c8317213989dc409f203d42e3a7
siddhardh4356 <siddhu4356@gmail.com>
2025-12-21 13:19:54 +0530

    chore: disable several TypeScript and React ESLint rules and add `react-hooks/exhaustive-deps` warning.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,10 +14,15 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "prefer-const": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
 
 export default eslintConfig;
+
 
